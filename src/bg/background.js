7ff93c4d092a46d6e8daebe9b257f9016525231b
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-01-31 01:11:39 -0500

    Initial commit.

---
+++ src/bg/background.js
@@ -0,0 +1,29 @@
+var parseOffsetCookie = function(offset) {
+
+	offset = parseInt(offset);
+
+	offset = isNaN(offset) ? 0 : offset;
+
+	return offset;
+
+};
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+
+		chrome.cookies.get(
+			{
+				"url": "http://ewaitlist.sundance.org/",
+				"name": "toff"
+			},
+			function(cookie) {
+
+				sendResponse({ "toff": parseOffsetCookie(cookie.value) });
+
+			}
+		);
+
+		return true;
+
+	}
+);
---
+++ src/inject/inject.js
@@ -0,0 +1,81 @@
+var timer_offset, jump_the_gun, open_time, join_btn, screeningTimer;
+
+var readyStateCheckInterval = setInterval(function() {
+
+	if (document.readyState === "complete") {
+
+		clearInterval(readyStateCheckInterval);
+
+		initialize();
+
+	}
+
+}, 10);
+
+var initialize = function() {
+
+	jump_the_gun = 200;
+	open_time    = document.querySelector('.wl-state-early > .livetime').dataset.epoch / 1000;
+	join_btn     = document.querySelector('#join-ui > a');
+
+	chrome.runtime.sendMessage({}, function(response) {
+
+		timer_offset = response.toff;
+
+	});
+
+	injectCheckbox();
+
+};
+
+var injectCheckbox = function() {
+
+	var container = document.createElement("label");
+
+	container.className = "ui-btn";
+
+	container.innerHTML = '<span class="ui-btn-inner"><input type="checkbox" id="auto-join"></input>Auto join the waitlist.</span>';
+
+	container.querySelector('#auto-join').addEventListener('change', setupAutoWaitlist, true);
+
+	var attach_to = document.querySelector('.wl-state-early');
+
+	attach_to.appendChild(container);
+
+};
+
+var setupAutoWaitlist = function(event) {
+
+	if ( event.target.checked ) {
+
+		attachCountdownWatcher();
+
+	} else {
+
+		removeCountdownWatcher();
+
+	}
+
+};
+
+var attachCountdownWatcher = function() {
+
+	screeningTimer = setInterval(function() {
+
+		var now = Math.ceil((new Date).getTime() / 1000) + timer_offset - jump_the_gun;
+console.log(now, open_time);
+		if ( now > open_time ) {
+
+			join_btn.click();
+
+		}
+
+	}, 200);
+
+};
+
+var removeCountdownWatcher = function() {
+
+	clearInterval(screeningTimer);
+
+};
